c732c37b2f34a6f776939d553b8d5faa85f05df6
fs65 <55656397+fs65@users.noreply.github.com>
2021-03-18 13:26:13 +0000

    final login

--- components/Register.tsx
+++ components/Register.tsx
@@ -11,30 +11,32 @@ import {
     Colors,
 } from 'react-native/Libraries/NewAppScreen';
 import { Resolver, Query, Mutation, Arg, UseMiddleware } from "type-graphql";
-import bcrypt from "bcrypt.js";
-import {user} from "../../entity/user";
-import {input} from "../modules/register_check"
-import {auth} from "../middle/auth";
-import {log} from "../middle/log";
-import {sendEmail} from "../util/sendEmail";
-import {createUrl} from "../util/createUrl";
+import bcrypt from "bcryptjs";
+import {User} from "../../entity/User";
+import {input} from "components/src/modules/user/register_check"
+import {auth} from "components/src/modules/middle/auth";
+import {log} from "components/src/modules/middle/log";
+import {sendEmail} from "components/src/modules/util/sendEmail";
+import {createUrl} from "components/src/modules/util/createUrl";
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
 
 @Resolver()
 export class RegisterResolver {
 
     @UseMiddleware(auth, log)
-    @Mutation(() => user)
+    @Mutation(() => User)
     async register(@Arg("data")
     {
       email,
       firstName,
       lastName,
       password
-    }: input): Promise<user> {
+    }: input): Promise<User> {
      
         const hashedPassword = await bcrypt.hash(password, 12);
 
-        const new_user = await user.create({
+        const new_user = await User.create({
 
             firstName,
             lastName,
@@ -43,9 +45,8 @@ export class RegisterResolver {
       }).save();
 
 
-      await sendEmail(email, await createUrl(user.id));
-     
-
+      await sendEmail(email, await createUrl(User.id));
+      
       return  (
         <View>
             <Text>E-Mail</Text>
@@ -58,13 +59,22 @@ export class RegisterResolver {
     } 
 }
 
-export default function change({ navigation }) {
+<Popup
+trigger={open => (
+  <button className="button">Move to Login - {open ? 'Opened' : 'Closed'}</button>
+)}
+position="right center"
+closeOnDocumentClick
+>
+<span> You have been sent an email to confirm your email address. Please click the link before trying to log in!! </span>
+</Popup>;
 
+export default function change({ navigation }) {
 
     return (
 
         <View>
-            <Text>Move to login</Text>
+            <Text>Login</Text>
             <Button
                 title="login"
                 onPress={() => navigation.navigate('Login')}

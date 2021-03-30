db1c6709b20b4ff36f0fb1a0d1aaf0f81dbf2160
moganli <morganli1216@gmail.com>
2021-03-30 18:33:53 +0100

    css change for imageView

--- components/ImageView.tsx
+++ components/ImageView.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Text, Dimensions, Image, ScrollView, StyleSheet } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+
 
 import CommentSection from './CommentSection';
 
@@ -12,8 +14,8 @@ export default function ImageView({ navigation, route }) {
     const image = route.params;
     return (
         <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
-            <TouchableOpacity onPress={() => navigation.navigate("Profile", { username: image.uploader })}>
-                <Text>{"by: " + image.uploader}</Text>
+            <TouchableOpacity style={styles.postername} onPress={() => navigation.navigate("Profile", { username: image.uploader })}>
+                <Text style={{fontSize:20,fontStyle:"italic",fontWeight:"bold",}}>{"by: " + image.uploader}</Text>
             </TouchableOpacity>
             < Image source={{ uri: image.uri }} style={styles.image} />
             <CommentSection image={image} />
@@ -36,4 +38,17 @@ const styles = StyleSheet.create({
         height: windowHeight * (1 / 16),
         margin: 2
     },
+    postername: {
+        alignItems: "center",
+
+        borderWidth: 0,
+        width: "98%",
+        borderRadius: 8,
+        backgroundColor: '#31a8bd',
+        // left:5,
+        marginHorizontal:5,
+        marginVertical:2,
+        // top:5,
+    },
+    
 })
